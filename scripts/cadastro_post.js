9a83b1e40d7a94c68828b966696ea57f67118bc6
henriquetorrescampos <henriquetorrescampos@gmail.com>
2024-08-19 21:21:12 -0300

    add new branch tela-home

--- scripts/cadastro_post.js
+++ scripts/cadastro_post.js
@@ -1,23 +1,21 @@
 function cadastrarPost(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    
+  const post = {
+    id: crypto.randomUUID(),
+    titulo: document.getElementById("titulo").value,
+    descricao: document.getElementById("descricao").value,
+    dataCriacao: new Date().toLocaleDateString(),
+    categoria: document.getElementById("categoria").value,
+    foto: document.getElementById("foto").value,
+  };
 
-    const post = {
-        id: Date.now(),
-        titulo: document.getElementById('titulo').value,
-        descricao: document.getElementById('descricao').value,
-        dataCriacao: new Date().toLocaleDateString(),  
-        categoria: document.getElementById('categoria').value,
-        foto: document.getElementById('foto').value
-    };
+  let posts = JSON.parse(localStorage.getItem("posts")) || [];
+  posts.push(post);
+  localStorage.setItem("posts", JSON.stringify(posts));
 
-    let posts = JSON.parse(localStorage.getItem('posts')) || [];
-    posts.push(post);
-    localStorage.setItem('posts', JSON.stringify(posts));
-
-    alert('Post salvo com sucesso!');
-    document.getElementById('postForm').reset();
+  alert("Post salvo com sucesso!");
+  document.getElementById("postForm").reset();
 }
 
-document.getElementById('postForm').addEventListener('submit', cadastrarPost);
+document.getElementById("postForm").addEventListener("submit", cadastrarPost);
