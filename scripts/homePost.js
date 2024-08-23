3c40ce116429898e5d5f5d0b2ef60a4e3e817758
henriquetorrescampos <henriquetorrescampos@gmail.com>
2024-08-23 18:50:21 -0300

    add new function to get object using localStorage

--- scripts/homePost.js
+++ scripts/homePost.js
@@ -1,7 +1,37 @@
 function loadPosts() {
-  const postsLocalStorage = JSON.parse(
-    localStorage.getItem("name-item-localStorage")
-  );
+  const postsLocalStorage = JSON.parse(localStorage.getItem("posts"));
+
+  const postList = document.getElementById("posts-in");
+
+  postsLocalStorage.forEach((each_post) => {
+    console.log(each_post.titulo);
+
+    const div = document.createElement("div");
+    div.classList.add("post-item");
+
+    const img = document.createElement("img");
+    img.setAttribute("width", "100px");
+    img.setAttribute("src", each_post.foto);
+
+    div.append(img);
+
+    const h3 = document.createElement("h3");
+    h3.innerText = each_post.titulo;
+
+    div.append(h3);
+
+    const description = document.createElement("span");
+    description.innerText = each_post.descricao;
+
+    div.append(description);
+
+    const currentePostDate = document.createElement("span");
+    currentePostDate.innerText = each_post.dataCriacao;
+
+    div.append(currentePostDate);
+
+    postList.append(div);
+  });
 }
 
 document.addEventListener("DOMContentLoaded", loadPosts);
