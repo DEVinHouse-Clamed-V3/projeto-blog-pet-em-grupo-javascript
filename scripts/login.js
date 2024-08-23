95f9c0bbdd6ec4242fede32c6863044e98609be2
Lucas Benkendorf <benklucass@gmail.com>
2024-08-23 18:06:39 -0300

    criado array e um find para buscar o login dentro dele

--- scripts/login.js
+++ scripts/login.js
@@ -1,23 +1,24 @@
-    
-        localStorage.setItem('email', '');
-        localStorage.setItem('password', '');
+const users = [
+    { email: 'admin1@exemplo.com', password: 'admin1' },
+    { email: 'admin2@exemplo.com', password: 'admin2' },
+    { email: 'admin3@exemplo.com', password: 'admin3' }
+];
 
-       
-        document.getElementById('loginForm').addEventListener('submit', function(event) {
-            event.preventDefault(); 
 
-     
-            const emailInput = document.getElementById('email').value;
-            const passwordInput = document.getElementById('password').value;
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
 
-            const storedEmail = localStorage.getItem('email');
-            const storedPassword = localStorage.getItem('password');
+ 
+    const emailInput = document.getElementById('email').value;
+    const passwordInput = document.getElementById('password').value;
 
-        
-            if(emailInput === storedEmail && passwordInput === storedPassword) {
-                alert('Login realizado com sucesso!');
-             
-                window.location.href = 'index.html';
-            } else {
-                alert('E-mail ou senha incorretos.');
-        }})
+  
+    const user = users.find(user => user.email === emailInput && user.password === passwordInput);
+
+    if (user) {
+        alert('Login realizado com sucesso!');
+        window.location.href = 'home.html';
+    } else {
+        alert('E-mail ou senha incorretos.');
+    }
+});
