a3f1d0b31321dd3683fd334f6691e410c9cb0282
frankberwald <berwald1997@outlook.com>
2024-08-25 17:34:37 -0300

    . Adicionado novas funções ao script como: h6 para título do post, e após o parágrafo, gerar data e hora  da criação do post.

--- scripts/viewpost.js
+++ scripts/viewpost.js
@@ -1,20 +1,27 @@
-
-  function generateDiv(imageUrl, textContent) {
-
-  const newDiv = document.createElement('div');
-  newDiv.className = 'content-div';
+function generateDiv(imageUrl, postTitleText, textContent, postNumber, creationDateText) {
+    const newDiv = document.createElement('div');
+    newDiv.className = 'content-div';
 
   const img = document.createElement('img');
   img.src = imageUrl || '../assets/error.png';
   img.alt = 'Imagem do Post';
 
+  const postTitle = document.createElement('h1');
+  postTitle.textContent = postTitleText || `Post ${postNumber}`;
+
   const paragraph = document.createElement('p');
   paragraph.textContent = textContent || 'Try refreshing the page';
 
+  const creationDate = document.createElement('h6');
+  const currentDate = new Date();
+  const formattedTime = currentDate.toLocaleTimeString();
+  const formattedDate = currentDate.toLocaleDateString();
+  creationDate.textContent = creationDateText || `Criado em: ${formattedDate} às ${formattedTime} `;
 
   newDiv.appendChild(img);
+  newDiv.appendChild(postTitle);
   newDiv.appendChild(paragraph);
-
+  newDiv.appendChild(creationDate);
 
   const outputContainer = document.getElementById('output-container');
   outputContainer.appendChild(newDiv);
@@ -24,19 +31,32 @@ document.getElementById('generate-button').addEventListener('click', function() 
   const outputContainer = document.getElementById('output-container');
   outputContainer.innerHTML = '';
 
-  const dataKeys = ['item1', 'item2', 'item3']; 
-  dataKeys.forEach(key => {
+  const dataKeys = ['item1', 'item2', 'item3', 'item4', 'item5'];
+  dataKeys.forEach((key, index) => {
       const imageUrl = localStorage.getItem(key + '_imageUrl');
+      const postTitle = localStorage.getItem(key + '_postTitle')
       const textContent = localStorage.getItem(key + '_textContent');
-      generateDiv(imageUrl, textContent);
+      const creationDateText = '';
+      generateDiv(imageUrl,postTitle, textContent, index + 1, creationDateText);
   });
 });
 
 localStorage.setItem('item1_imageUrl', '../assets/pet1.webp');
+localStorage.setItem('item1_postTitle', '');
 localStorage.setItem('item1_textContent', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit repellat minus architecto incidunt nesciunt. Id, reprehenderit laudantium praesentium modi, porro facilis tempore, hic adipisci repellat debitis molestias obcaecati ex dignissimos..');
 
 localStorage.setItem('item2_imageUrl', '../assets/pet5.jpg');
+localStorage.setItem('item2_postTitle', '');
 localStorage.setItem('item2_textContent', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit repellat minus architecto incidunt nesciunt. Id, reprehenderit laudantium praesentium modi, porro facilis tempore, hic adipisci repellat debitis molestias obcaecati ex dignissimos..');
 
-localStorage.setItem('item3_imageUrl', '');
-localStorage.setItem('item3_textContent', 'No image available for this item.');
+localStorage.setItem('item3_imageUrl', '../assets/gatinho.jpg');
+localStorage.setItem('item3_postTitle', '');
+localStorage.setItem('item3_textContent', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit repellat minus architecto incidunt nesciunt. Id, reprehenderit laudantium praesentium modi, porro facilis tempore, hic adipisci repellat debitis molestias obcaecati ex dignissimos..');
+
+localStorage.setItem('item4_imageUrl', '../assets/pet6.jpg');
+localStorage.setItem('item4_postTitle', '');
+localStorage.setItem('item4_textContent', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit repellat minus architecto incidunt nesciunt. Id, reprehenderit laudantium praesentium modi, porro facilis tempore, hic adipisci repellat debitis molestias obcaecati ex dignissimos..');
+
+localStorage.setItem('item5_imageUrl', '');
+localStorage.setItem('item5_postTitle', '404: not found :(');
+localStorage.setItem('item5_textContent', 'No image available for this item.');
